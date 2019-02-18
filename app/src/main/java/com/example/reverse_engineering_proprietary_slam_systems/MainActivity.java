1760483fdb0136d88086b4bac8c1e33d6ffd34c3
hasretg <hasret.g46@gmail.com>
2019-02-18 13:33:10 +0100

    test

--- app/src/main/java/com/example/reverse_engineering_proprietary_slam_systems/MainActivity.java
+++ app/src/main/java/com/example/reverse_engineering_proprietary_slam_systems/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.reverse_engineering_proprietary_slam_systems;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.Console;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("test");
     }
 }
