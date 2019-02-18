7f0f9974b6d66b02a43c93eee9f670de5905f8ed
hasretg <hasret.g46@gmail.com>
2019-02-18 16:11:59 +0100

    Create first ARcore app. Being able to place a sphere to a flat surface. Add dependencies for the camera to the manifest file.

--- app/src/main/java/com/example/reverse_engineering_proprietary_slam_systems/MainActivity.java
+++ app/src/main/java/com/example/reverse_engineering_proprietary_slam_systems/MainActivity.java
@@ -3,14 +3,40 @@ package com.example.reverse_engineering_proprietary_slam_systems;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.io.Console;
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.MaterialFactory;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+import com.google.ar.sceneform.ux.ArFragment;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("test");
+
+        ArFragment arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+
+            MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.RED))
+                    .thenAccept(material -> {
+                        ModelRenderable renderable = ShapeFactory.makeSphere(1.0f, new Vector3(0f, 1f, 1f), material);
+
+                        AnchorNode anchorNode = new AnchorNode(anchor);
+                        anchorNode.setRenderable(renderable);
+                        arFragment.getArSceneView().getScene().addChild(anchorNode);
+                    });
+        });
+
     }
+
 }
