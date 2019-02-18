f55ef3df7f1dc610433107cec282cf82803cf33b
Hasret Gümgümcü <hasret@Hasrets-MacBook-Pro.local>
2019-02-18 13:20:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/reverse_engineering_proprietary_slam_systems/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.reverse_engineering_proprietary_slam_systems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
