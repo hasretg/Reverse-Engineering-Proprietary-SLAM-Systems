73f775ae66566e38e08d9c32a38666568d5920ea
hasretg <hasret.g46@gmail.com>
2019-02-21 12:46:14 +0100

    New button functionalities, cleaning code

--- app/src/main/java/com/example/reverse_engineering_proprietary_slam_systems/MainActivity.java
+++ app/src/main/java/com/example/reverse_engineering_proprietary_slam_systems/MainActivity.java
@@ -8,7 +8,6 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -24,18 +23,21 @@ import com.google.ar.sceneform.ux.ArFragment;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
 
     /* Define global variables */
-    protected long startTime;
-    protected static String TEXT_FILE_NAME = "cameraLoc.txt";
-    OutputStreamWriter myStreamWriter;
-    String tStep = "";
-    public ArFragment arFragment;
-    public Button recordButton;
-    public Button myStopButton;
+    private static String TEXT_FILE_NAME = "arCameraPose.txt";
+    private String arCameraPoseText = "";
+    protected OutputStreamWriter myStreamWriter;
+    private long startTime;
+    private ArFragment arFragment;
+    private Button recordButton;
+    private boolean recBtnClicked = false;
+    private Button poseCaptureButton;
+    private boolean captBtnClicked = false;
     private VideoRecorder videoRecorder;
 
     @Override
@@ -43,42 +45,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Defining elements from the UI */
+        // Defining elements from the UI
         arFragment = (ArFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.arFragment);
         recordButton = findViewById(R.id.recButton);
-        myStopButton = findViewById(R.id.myStopButton);
+        poseCaptureButton = findViewById(R.id.poseCaptureButton);
 
-        // Initialize the VideoRecorder.
+        // Initialize the VideoRecorder
         videoRecorder = new VideoRecorder();
         int orientation = getResources().getConfiguration().orientation;
-        videoRecorder.setVideoQuality(CamcorderProfile.QUALITY_2160P, orientation);
+        videoRecorder.setVideoQuality(CamcorderProfile.QUALITY_1080P, orientation);
         videoRecorder.setSceneView(arFragment.getArSceneView());
+        videoRecorder.setFrameRate(5);
 
-        recordButton.setOnClickListener(this::toggleRecording);
-        recordButton.setEnabled(true);
+        // Store timestamp when application is executed as start time
+        startTime = System.currentTimeMillis();
 
-        /* Creating a text-file to save camera pose with the timestamps from the AR application
-        * Path of the file: data/data/com.example.reverse_eng../files/textFile
-        */
-        try{
-            myStreamWriter = new OutputStreamWriter(openFileOutput(TEXT_FILE_NAME, 0));
-
-        }catch(Throwable t){
-            Log.e("OutputStreamWriterException", "OutputStreamWriter not possible.");
-        }
-
-        /* Store timestamp when application is executed as start time */
-        startTime= System.currentTimeMillis();
-
-        /* TapArPlaneListener: When plane in AR recognized and taped, create a sphere at the taped 
-        * location */
+        /*
+         * When plane in AR recognized and taped, create a sphere at the taped location
+         */
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+
             Anchor anchor = hitResult.createAnchor();
 
             MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.RED))
                     .thenAccept(material -> {
-                        ModelRenderable renderable = ShapeFactory.makeSphere(0.1f, 
+                        ModelRenderable renderable = ShapeFactory.makeSphere(0.1f,
                                 new Vector3(0f, 0f, 0f), material);
 
                         AnchorNode anchorNode = new AnchorNode(anchor);
@@ -87,45 +79,67 @@ public class MainActivity extends AppCompatActivity {
                     });
         });
 
+        /*
+         * Handle video recording
+         */
+        recordButton.setOnClickListener(v -> {
 
-        /* OnClickListener: Write pose and timestamp in a txt-file */
-        //recordButton.setOnClickListener(v -> {
-
-
-       // });
-
-
-        /* Save txt-file when StopButton is clicked */
-        myStopButton.setOnClickListener(v -> {
-            Log.v("test1", "Timestamp: " + tStep );
-/*
-            try{
-                myStreamWriter.close();
-            }catch(Throwable t){
-                Log.w("StreamWriterWarning", "Text file could not be stored.");
+            recBtnClicked = !recBtnClicked;
+            this.toggleRecording();
+            if (recBtnClicked) {
+                recordButton.setText("STOP Recording");
+            } else {
+                recordButton.setText("START Recording");
             }
-            */
         });
 
+        /*
+         * Handle camera pose
+         */
+        poseCaptureButton.setOnClickListener(v -> {
+
+            Log.v("test1", "Timestamp: " + arCameraPoseText);
+            captBtnClicked = !captBtnClicked;
+
+            if (captBtnClicked) {
+                try {
+                    myStreamWriter = new OutputStreamWriter(openFileOutput(TEXT_FILE_NAME +
+                            Calendar.getInstance().getTime().toString(), 0));
+                    poseCaptureButton.setText("STOP Capture Pose");
+                } catch (Throwable t) {
+                    Log.e("OutputStreamWriterExcep", "Text file could not be generated.");
+                }
+            } else {
+                try {
+                    this.writeToFile(arCameraPoseText);
+                    myStreamWriter.close();
+                    poseCaptureButton.setText("START Capture Pose");
+                } catch (Throwable t) {
+                    Log.w("StreamWriterWarning", "Text file could not be saved.");
+                }
+            }
+        });
+
+        /*
+         * Get camera pose for every frame
+         */
         arFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
 
             Vector3 currCameraPose = arFragment.getArSceneView().getScene().getCamera()
                     .getWorldPosition();
-            long currTime= System.currentTimeMillis() - startTime;
-            String dataToStore = "" + currTime + ":  " + currCameraPose.toString() + "\n";
-            //writeToFile(dataToStore, myStreamWriter);
-            tStep += ""+frameTime.getDeltaSeconds()+"  Pos: "+currCameraPose.toString()+"\n";
+            long currTime = System.currentTimeMillis() - startTime;
+            arCameraPoseText += "" + currTime + "[ Delta sec: " + frameTime.getDeltaSeconds() +
+                    " ]" + "  Pos: " + currCameraPose.toString() + "\n";
         });
     }
 
     /*
      * Used as a handler for onClick, so the signature must match onClickListener.
      */
-    private void toggleRecording(View unusedView) {
+    private void toggleRecording() {
 
         boolean recording = videoRecorder.onToggleRecord();
-        if (recording) {
-        } else {
+        if (!recording) {
             String videoPath = videoRecorder.getVideoPath().getAbsolutePath();
             Toast.makeText(this, "Video saved: " + videoPath, Toast.LENGTH_SHORT).show();
             Log.d("Video status", "Video saved: " + videoPath);
@@ -139,11 +153,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /* Write a string in a given file */
-    private void writeToFile(String data, OutputStreamWriter streamWriter) {
+    /*
+     * Write a string in a given file
+     */
+    private void writeToFile(String data) {
+
         if (isExternalStorageWritable()){
             try {
-                streamWriter.write(data);
+                myStreamWriter.write(data);
 
                 Toast.makeText(this, "File saved" ,Toast.LENGTH_SHORT).show();
             }
@@ -163,8 +180,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /* Checks if external storage is available for read and write */
+    /*
+     * Checks if external storage is available for read and write
+     */
     public boolean isExternalStorageWritable(){
+
         String state = Environment.getExternalStorageState();
         if (Environment.MEDIA_MOUNTED.equals(state)) {
             return true;
